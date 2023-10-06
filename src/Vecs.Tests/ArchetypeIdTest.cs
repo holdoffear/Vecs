47190aa26f63b57e598f68f8446adda2cb52bc47
holdoffear <holdoffear@protonmail.com>
2023-10-06 12:32:27 +0200

    Updated ArchetypeId tests

--- src/Vecs.Tests/ArchetypeIdTest.cs
+++ src/Vecs.Tests/ArchetypeIdTest.cs
@@ -6,14 +6,39 @@ namespace Vecs.Tests
     public class ArchetypeIdTest
     {
         [TestMethod]
+        [DataRow(new Type[]{typeof(int), typeof(string), typeof(bool)}, typeof(int))]
+        [DataRow(new Type[]{typeof(int), typeof(string), typeof(bool)}, typeof(bool))]
+        [DataRow(new Type[]{typeof(int), typeof(string), typeof(bool)}, typeof(string))]
+        public void Contains_SingleType_ReturnsTrue(Type[] input, Type expected)
+        {
+            ArchetypeId archetypeId = new ArchetypeId(input);
+            bool result = archetypeId.Contains(expected);
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
         [DataRow(new Type[0], 0)]
         [DataRow(new Type[]{typeof(int), typeof(string), typeof(bool)}, 3)]
         [DataRow(new Type[]{typeof(int), typeof(string)}, 2)]
-        public void Count_ReturnsSingleNumber(Type[] input, int expected)
+        public void Count_TypeArray_ReturnsSingleNumber(Type[] input, int expected)
         {
             ArchetypeId archetypeId = new ArchetypeId(input);
             int result = archetypeId.Count;
             Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        [DataRow(new Type[]{typeof(int), typeof(string), typeof(bool)})]
+        [DataRow(new Type[]{typeof(int), typeof(string)})]
+        [DataRow(new Type[]{})]
+        public void GetTypes_TypeArray_ReturnsTypeArray(Type[] input)
+        {
+            ArchetypeId archetypeId = new ArchetypeId(input);
+            Type[] result = archetypeId.GetTypes();
+            Assert.AreEqual(input.Length, result.Length);
+            for (int i = 0; i <= input.Length; i++)
+            {
+                bool hasType = result.Contains(input[i]);
+                Assert.IsTrue(hasType);
+            }
         }
     }
 }
