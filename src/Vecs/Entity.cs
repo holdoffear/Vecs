dc19f4d38c8f631bc38d279018ff35d064aee71c
holdoffear <holdoffear@protonmail.com>
2023-10-07 10:50:54 +0200

    Implemented IEquatable for Entity

--- src/Vecs/Entity.cs
+++ src/Vecs/Entity.cs
@@ -1,11 +1,15 @@
 namespace Vecs
 {
-    public struct Entity
+    public struct Entity : IEquatable<Entity>
     {
         public ArchetypeId ArchetypeId {get;}
         public Entity(ArchetypeId archetypeId)
         {
             this.ArchetypeId = archetypeId;
+        }
+        public bool Equals(Entity other)
+        {
+            return ArchetypeId.Equals(other.ArchetypeId);
         }
     }
 }
