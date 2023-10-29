a8c21b0e92b351e5b7b51f176f599b16bc37d402
holdoffear <holdoffear@protonmail.com>
2023-10-29 15:16:14 +0200

    Updated example code

--- examples/Floating_Cubes/Main.cs
+++ examples/Floating_Cubes/Main.cs
@@ -1,12 +1,64 @@
-using Godot;
 using System;
+using System.Diagnostics;
+using Godot;
+using Vecs;
 
 public partial class Main : Node3D
 {
+	World world = new World();
+	Query query;
+	int instances = 10000;
+	Rid meshInstance3D;
 	public override void _Ready()
 	{
+		meshInstance3D = new BoxMesh().GetRid();
+		query = new Query(world);
+		for (int i = 0; i < instances; i++)
+		{
+			Transform3D transform3D = new Transform3D(Basis.Identity, Vector3.Up);
+			InstanceRid instance = new InstanceRid(RenderingServer.InstanceCreate());
+
+			Entity entity = world.CreateEntity();
+			entity = query.AddComponent(entity, meshInstance3D);
+			entity = query.AddComponent(entity, transform3D);
+			entity = query.AddComponent(entity, instance);
+
+			RenderingServer.InstanceSetScenario(instance.Rid, GetWorld3D().Scenario);
+            RenderingServer.InstanceSetBase(instance.Rid, meshInstance3D);
+            RenderingServer.InstanceSetTransform(instance.Rid, transform3D);
+		}
+		query.With(new Type[]{typeof(InstanceRid), typeof(Transform3D)});
 	}
 	public override void _Process(double delta)
 	{
+		Stopwatch stopwatch = new Stopwatch();
+		stopwatch.Start();
+		query.Foreach((ref InstanceRid instanceRid, ref Transform3D transform3D) => 
+		{
+			transform3D = transform3D.Translated(GD.RandRange(-1, 1) * new Godot.Vector3(GD.Randf(), GD.Randf(), GD.Randf()));
+			RenderingServer.InstanceSetTransform(instanceRid.Rid, transform3D);
+		});
+		stopwatch.Stop();
+		GD.Print(stopwatch.ElapsedMilliseconds);
 	}
+}
+
+struct InstanceRid : IEquatable<InstanceRid>
+{
+	public Rid Rid;
+	public InstanceRid(Rid rid)
+	{
+		this.Rid = rid;
+	}
+
+    public int CompareTo(object obj)
+    {
+		return (int)(this.Rid.Id - ((InstanceRid)obj).Rid.Id);
+    }
+
+
+    public bool Equals(InstanceRid other)
+    {
+        return this.Rid.Equals(other.Rid);
+    }
 }
