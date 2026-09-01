5326a478c17581013cea0a459d66b20b70f53087
holdoffear <136822557+holdoffear@users.noreply.github.com>
2026-09-01 18:49:42 +0200

    Updated tests

--- tests/QueryTest.cs
+++ tests/QueryTest.cs
@@ -11,6 +11,19 @@ public class QueryTest
             yield return (world, entity, count-i);
         }
     }
+    public static IEnumerable<(World world, Entity entity, int elementA, char elementB)> CreateQueryTwoComponentsData(int count)
+    {
+        World world = new(count);
+        for (int i = 0; i < count; i++)
+        {
+            world.CreateEntity(i, 1.0d);
+        }
+        for (int i = 0; i < count; i++)
+        {
+            Entity entity = world.CreateEntity(i, 'c');
+            yield return (world, entity, i, 'c');
+        }
+    }
     [TestMethod]
     [DynamicData(nameof(CreateQueryData), dynamicDataSourceArguments: 1)]
     [DynamicData(nameof(CreateQueryData), dynamicDataSourceArguments: 100)]
@@ -24,5 +37,21 @@ public class QueryTest
         });
         T value = world.GetComponent<T>(entity);
         Assert.AreEqual(num, value);
+    }
+    [TestMethod]
+    [DynamicData(nameof(CreateQueryTwoComponentsData), dynamicDataSourceArguments: 1)]
+    [DynamicData(nameof(CreateQueryTwoComponentsData), dynamicDataSourceArguments: 100)]
+    public void CreateQuery<T1, T2>(World world, Entity entity, T1 elementA, T2 elementB)
+    {
+        Query query = world.CreateQuery();
+        query.Get<T1, T2>();
+        query.Foreach((ref T1 componentA, ref T2 componentB) =>
+        {
+            componentA = elementA;
+            componentB = elementB;
+        });
+        T1 valueA = world.GetComponent<T1>(entity);
+        T2 valueB = world.GetComponent<T2>(entity);
+        Assert.AreEqual(elementA, valueA);
     }
 }
