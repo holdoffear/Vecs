bf94dc90ab992e6f12dbb2ab6d2ca9ac5f390de4
holdoffear <136822557+holdoffear@users.noreply.github.com>
2026-09-04 08:52:10 +0200

    Added query with iterating over the entities as well

--- src/Query.Foreach.cs
+++ src/Query.Foreach.cs
@@ -26,6 +26,20 @@ public partial class Query
             }
         }
     }
+    public void Foreach<T1, T2>(OperationWithEntity<T1, T2> operation)
+    {
+        Archetype[] archetypes = World.GetArchetypes(GetBits, WithBits, ExcludeBits);
+        foreach (Archetype archetype in archetypes)
+        {
+            Span<Entity> entities = archetype.GetEntitiesAsSpan();
+            Span<T1> componentsA = archetype.GetComponentsAsSpan<T1>();
+            Span<T2> componentsB = archetype.GetComponentsAsSpan<T2>();
+            for (int i = 0; i < componentsA.Length; i++)
+            {
+                operation(ref entities[i], ref componentsA[i], ref componentsB[i]);
+            }
+        }
+    }
     public void Foreach<T1, T2, T3>(Operation<T1, T2, T3> operation)
     {
         Archetype[] archetypes = World.GetArchetypes(GetBits, WithBits, ExcludeBits);
--- src/Query.Operation.cs
+++ src/Query.Operation.cs
@@ -3,5 +3,6 @@ public partial class Query
 {
     public delegate void Operation<T1>(ref T1 componentA);
     public delegate void Operation<T1, T2>(ref T1 componentA, ref T2 componentB);
+    public delegate void OperationWithEntity<T1, T2>(ref Entity entity, ref T1 componentA, ref T2 componentB);
     public delegate void Operation<T1, T2, T3>(ref T1 componentA, ref T2 componentB, ref T3 componentC);
 }
