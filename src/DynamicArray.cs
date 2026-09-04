658b3731775ccf2181c00c5fb62d02c566ed2bc8
holdoffear <136822557+holdoffear@users.noreply.github.com>
2026-09-04 12:10:56 +0200

    added automatic resizing to dynamic array

--- src/DynamicArray.cs
+++ src/DynamicArray.cs
@@ -3,6 +3,7 @@ public struct DynamicArray<T> : IEnumerable<T>
 where T : struct
 {
     private int NextIndex = 0;
+    private int Size = 0;
     private T[] Data;
     public int Length
     {
@@ -11,12 +12,24 @@ where T : struct
     public DynamicArray(int count)
     {
         Data = new T[count];
+        Size = Data.Length;
     }
     public ref T this[int index]
     {
         get => ref Data[index];
     }
-    public void Add(T element) => Data[NextIndex++] = element;
+    public void Add(T element)
+    {
+        if (NextIndex < Size)
+        {
+            Data[NextIndex++] = element;
+        }
+        else
+        {
+            Resize();
+            Data[NextIndex++] = element;
+        }
+    }
     public Span<T> AsSpan() => new(Data, 0, Length);
     public IEnumerator<T> GetEnumerator()
     {
@@ -32,5 +45,15 @@ where T : struct
         {
             Data[index] = Data[--NextIndex];
         }
+    }
+    public void Resize()
+    {
+        Size *= 2;
+        Array.Resize(ref Data, Size);
+    }
+    public void Shrink()
+    {
+        Size = NextIndex;
+        Array.Resize(ref Data, Size);
     }
 }
