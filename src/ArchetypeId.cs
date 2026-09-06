7dd049a6e240457f7fbed98478765bdcb80da78e
holdoffear <136822557+holdoffear@users.noreply.github.com>
2026-09-06 17:56:05 +0200

    Implemented ArchetypeId IEquatable<T>

--- src/ArchetypeId.cs
+++ src/ArchetypeId.cs
@@ -1,6 +1,5 @@
 namespace Vecs;
-public struct ArchetypeId
-//  : IEqualityOperators<ArchetypeId, Archetype, bool>
+public struct ArchetypeId : IEquatable<ArchetypeId>
 {
     public int Id = -1;
     public ArchetypeId(int id)
@@ -11,6 +10,9 @@ public struct ArchetypeId
     {
         Id = archetypeId.Id | bitwiseId;
     }
-    // public static bool operator ==(ArchetypeId left, Archetype right) => left.Id == right.ArchetypeId.Id;
-    // public static bool operator !=(ArchetypeId left, Archetype right) => left.Id != right.ArchetypeId.Id;
+    public override bool Equals(object? obj) => obj is ArchetypeId archetypeId && Equals(archetypeId);
+    public bool Equals(ArchetypeId other) => Id.Equals(other.Id);
+    public static bool operator ==(ArchetypeId left, ArchetypeId right) => left.Equals(right);
+    public static bool operator !=(ArchetypeId left, ArchetypeId right) => !left.Equals(right);
+    public override int GetHashCode() => Id.GetHashCode();
 }
