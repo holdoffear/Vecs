14839b11195ae52b7241273fd13a9f563fe1f47e
holdoffear <136822557+holdoffear@users.noreply.github.com>
2026-09-09 16:09:38 +0200

    implemented 8 of each Query foreach and Query operation

--- src/Query.Operation.cs
+++ src/Query.Operation.cs
@@ -2,7 +2,11 @@ namespace Vecs;
 public partial class Query
 {
     public delegate void Operation<T1>(ref T1 componentA);
-    public delegate void Operation<T1, T2>(ref T1 componentA, ref T2 componentB);
-    public delegate void OperationWithEntity<T1, T2>(ref Entity entity, ref T1 componentA, ref T2 componentB);
-    public delegate void Operation<T1, T2, T3>(ref T1 componentA, ref T2 componentB, ref T3 componentC);
+	public delegate void Operation<T1, T2>(ref T1 componentA, ref T2 componentB);
+	public delegate void Operation<T1, T2, T3>(ref T1 componentA, ref T2 componentB, ref T3 componentC);
+	public delegate void Operation<T1, T2, T3, T4>(ref T1 componentA, ref T2 componentB, ref T3 componentC, ref T4 componentD);
+	public delegate void Operation<T1, T2, T3, T4, T5>(ref T1 componentA, ref T2 componentB, ref T3 componentC, ref T4 componentD, ref T5 componentE);
+	public delegate void Operation<T1, T2, T3, T4, T5, T6>(ref T1 componentA, ref T2 componentB, ref T3 componentC, ref T4 componentD, ref T5 componentE, ref T6 componentF);
+	public delegate void Operation<T1, T2, T3, T4, T5, T6, T7>(ref T1 componentA, ref T2 componentB, ref T3 componentC, ref T4 componentD, ref T5 componentE, ref T6 componentF, ref T7 componentG);
+	public delegate void Operation<T1, T2, T3, T4, T5, T6, T7, T8>(ref T1 componentA, ref T2 componentB, ref T3 componentC, ref T4 componentD, ref T5 componentE, ref T6 componentF, ref T7 componentG, ref T8 componentH);
 }
