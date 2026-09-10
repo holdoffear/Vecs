e9388a79bcac1ddfaeac0b77f814544f664d4233
holdoffear <136822557+holdoffear@users.noreply.github.com>
2026-09-10 11:43:14 +0200

    updated entity constructor to be internal

--- src/Entity.cs
+++ src/Entity.cs
@@ -1,19 +1,13 @@
 namespace Vecs;
-// public struct Entity : IEquatable<Entity>
-// {
-//     public readonly EntityId EntityId;
-//     public readonly ArchetypeId ArchetypeId;
-//     public readonly int Index = -1;
-//     internal Entity(EntityId entityId, ArchetypeId archetypeId, int index)
-//     {
-//         EntityId = entityId;
-//         ArchetypeId = archetypeId;
-//         Index = index;
-//     }
-//     public override bool Equals(object? obj) => obj is Entity entityId && Equals(entityId);
-//     public bool Equals(Entity other) => EntityId.Equals(other.EntityId);
-//     public static bool operator ==(Entity left, Entity right) => left.Equals(right);
-//     public static bool operator !=(Entity left, Entity right) => !left.Equals(right);
-//     public override int GetHashCode() => EntityId.GetHashCode();
-// }
-public readonly record struct Entity(EntityId EntityId, ArchetypeId ArchetypeId, int Index = -1);
+public readonly record struct Entity
+{
+    public readonly EntityId EntityId;
+    public readonly ArchetypeId ArchetypeId;
+    public readonly int Index = -1;
+    internal Entity(EntityId entityId, ArchetypeId archetypeId, int index)
+    {
+        EntityId = entityId;
+        ArchetypeId = archetypeId;
+        Index = index;
+    }
+}
